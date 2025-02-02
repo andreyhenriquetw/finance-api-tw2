d567ac316f30a5cd3bc31ea04194c96f28a4bb6d
andreyhtw <andreyhenriquetw3@gmail.com>
2025-02-02 14:22:33 -0300

    feat: add helper function to user controllers

--- src/controllers/helpers/http.js
+++ src/controllers/helpers/http.js
@@ -1,16 +1,12 @@
-export const badRequest = (body) => {
-    return {
-        statusCode: 400,
-        body,
-    }
-}
+export const badRequest = (body) => ({
+    statusCode: 400,
+    body,
+})
 
-export const created = (body) => {
-    return {
-        statusCode: 201,
-        body,
-    }
-}
+export const created = (body) => ({
+    statusCode: 201,
+    body,
+})
 
 export const serverError = () => ({
     statusCode: 500,
