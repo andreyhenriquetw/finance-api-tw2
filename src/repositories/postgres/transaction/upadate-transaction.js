0e5fa51f8702885cf63d824965e5bc58f3e46fbc
andreyhtw <andreyhenriquetw3@gmail.com>
2025-02-24 19:40:41 -0300

    feat: add PostgresUpdateTransactionRepository

--- src/repositories/postgres/transaction/upadate-transaction.js
+++ src/repositories/postgres/transaction/upadate-transaction.js
@@ -1,23 +1,23 @@
 import { PostgresHelper } from '../../../db/postgres/helper.js'
 
-export class PostgresUpdateUserRepository {
-    async execute(userId, updateParams) {
+export class PostgresUpdateTransactionRepository {
+    async execute(userId, updateTransactionParams) {
         const updateFields = []
         const updateValues = []
 
-        Object.keys(updateParams).forEach((key) => {
+        Object.keys(updateTransactionParams).forEach((key) => {
             updateFields.push(`${key} = $${updateValues.length + 1}`)
-            updateValues.push(updateParams[key])
+            updateValues.push(updateTransactionParams[key])
         })
 
         updateValues.push(userId)
 
         const updateQuery = `
-        UPDATE users
-        SET ${updateFields.join(', ')}
-        WHERE id = $${updateValues.length}
-        RETURNING *
-      `
+            UPDATE transactions
+            SET ${updateFields.join(', ')}
+            WHERE id = $${updateValues.length}
+            RETURNING *
+          `
 
         const updatedUser = await PostgresHelper.query(
             updateQuery,
